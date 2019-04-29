84dfdb888cb8e6a484d695790b664ce0111d938c
yuanwansong <yuanwansong@woda.ink>
2019-04-29 20:15:54 +0800

    更新demo

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom';
 
 const classComp = React.createElement(
     class extends Component {
+        state = {
+            showText: 'level4-p2'
+        }
+
+        handleClick = () => {
+            this.setState({
+                showText: 'level4-p2' + new Date().getTime()
+            });
+        }
+
         render() {
             return React.createElement('div', {
                 className: 'level1',
                 key: 'level1'
             }, [
-                    React.createElement('button', { key: 'level2-btn', className: 'level2-btn' }, 'BUTTON'),
+                    React.createElement('button', { key: 'level2-btn', className: 'level2-btn', onClick: this.handleClick }, 'BUTTON'),
                     React.createElement('div', { key: 'level2-div1', className: 'level2-div1' },
                         React.createElement('div', { key: 'level3-div', className: 'level3-div' }, [
                             React.createElement('p', { key: 'level4-p1', className: 'level4-p1' }, 'level4-p1'),
-                            React.createElement('p', { key: 'level4-p2', className: 'level4-p2' }, 'level4-p2'),
+                            //  React.createElement('p', { key: 'level4-p2', className: 'level4-p2' }, 'level4-p2'),
+                            React.createElement('p', { key: 'level4-p2', className: 'level4-p2' }, this.state.showText),
                         ])
                     ),
                     React.createElement('div', { key: 'level2-div2', className: 'level2-div2' },
